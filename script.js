9856d48e9a591f99761d62ee885090b2acb2b5b2
arinsukmagoe <110323284+arinsukmagoe@users.noreply.github.com>
2026-07-31 22:42:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Bloom Cycle Ready 🌸");
+const card = document.getElementById("card");
+const overlay = document.getElementById("overlay");
+const cancel = document.getElementById("cancel");
+
+card.addEventListener("click", () => {
+    overlay.style.display = "flex";
+});
+
+cancel.addEventListener("click", () => {
+    overlay.style.display = "none";
+});
+
+// klik area gelap untuk menutup popup
+overlay.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+        overlay.style.display = "none";
+    }
+});
